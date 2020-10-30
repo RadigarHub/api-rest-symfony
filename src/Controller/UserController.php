a16d85c72d6ea5b5e61d78590cebb6ceaeb175d3
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-30 09:05:52 +0100

    Class 252

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
-     */
+
     public function index(): Response
     {
         return $this->json([
--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -4,13 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class VideoController extends AbstractController
 {
-    /**
-     * @Route("/video", name="video")
-     */
+
     public function index(): Response
     {
         return $this->json([
